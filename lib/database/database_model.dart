ac1d0b73cf6e116477386fa8df8894bedb0b197d
Etukudo Emmanuel <etukzbong@gmail.com>
2023-10-29 05:45:15 +0100

    setup database and tables

---
+++ lib/database/database_model.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:sqfentity/sqfentity.dart';
+import 'package:sqfentity_gen/sqfentity_gen.dart';
+import 'package:http/http.dart' as http;
+
+part 'database_model.g.dart';
+
+// tables
+const SqfEntityTable tableProducts = SqfEntityTable(
+    tableName: 'products',
+    primaryKeyName: 'id',
+    useSoftDeleting: false,
+    primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+    fields: [
+      SqfEntityField('name', DbType.text),
+      SqfEntityField('quantity', DbType.integer),
+      SqfEntityField('cost_price', DbType.real),
+      SqfEntityField('selling_price', DbType.real),
+      SqfEntityField('product_image', DbType.text),
+    ]);
+
+const seqIdentity = SqfEntitySequence(
+  sequenceName: 'identity',
+);
+
+// database
+@SqfEntityBuilder(myDbModel)
+const myDbModel = SqfEntityModel(
+    modelName: 'MyAppDatabaseModel',
+    databaseName: 'product_manager',
+    sequences: [seqIdentity],
+    databaseTables: [tableProducts]);
